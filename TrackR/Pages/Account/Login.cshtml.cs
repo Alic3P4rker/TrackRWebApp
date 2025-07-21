d3c15a68f239c5843a95aa3ff9a2277f71ef0d1d
LynnP4rker <avchambers004@gmail.com>
2025-07-21 10:49:36 +0100

    creating cookie, access token and refresh token

--- TrackR/Pages/Account/Login.cshtml.cs
+++ TrackR/Pages/Account/Login.cshtml.cs
@@ -1,4 +1,8 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
 using MediatR;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using TrackR.Commands;
@@ -22,7 +26,30 @@ public class LoginModel : PageModel
     public async Task<IActionResult> OnPost()
     {
         LoginCommand loginCommand = new LoginCommand(model.email, model.password);
-        await _mediator.Send(loginCommand);
+        Token token = await _mediator.Send(loginCommand);
+
+        Response.Cookies.Append("accessToken", token.accessToken, new CookieOptions
+        {
+            HttpOnly = true,
+            Secure = false,
+            SameSite = SameSiteMode.Strict,
+            Expires = DateTimeOffset.UtcNow.AddMinutes(10)
+        });
+
+        Response.Cookies.Append("refreshToken", token.refreshToken, new CookieOptions
+        {
+            HttpOnly = true,
+            Secure = false,
+            SameSite = SameSiteMode.Strict,
+        });
+
+        var handler = new JwtSecurityTokenHandler();
+        var jwt = handler.ReadJwtToken(token.accessToken);
+        var claims = jwt.Claims.ToList();
+
+        var claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+        await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme,
+            new ClaimsPrincipal(claimsIdentity));
 
         return RedirectToPage("/Index");
     }
